8129c0460741aebfd0ea8710bca2907d8a87f0a2
Baraa <178164194+AlbaraaAloush@users.noreply.github.com>
2025-09-18 14:09:29 +0300

    feat(logic.js): add getHumanChoice()
    This function prompts the player to enter rock, paper, or scissors.

--- logic.js
+++ logic.js
@@ -1,6 +1,27 @@
 function getComputerChoice() {
-
   const choice = Math.floor(Math.random() * 3);
-  return (choice === 0)? "rock":(choice === 1)? "paper":"scissors";
+  return choice === 0 ? "rock" : choice === 1 ? "paper" : "scissors";
 }
-alert(getComputerChoice());
+
+function getHumanChoice() {
+  while (true) {
+    const input = prompt("Enter your choice (rock, paper, or scissors):", "");
+
+    // check is user pressed cancel
+    if (input === null) {
+      alert("Game cancelled.");
+      return null;
+    }
+
+    // converting user's input to lowercase before handling it
+    const choice = input.toLowerCase().trim();
+
+    if (choice === "rock" || choice === "paper" || choice === "scissors") {
+      return choice;
+    } else {
+      alert("Invalid input, please try again.");
+    }
+  }
+}
+
+let x = getHumanChoice();
